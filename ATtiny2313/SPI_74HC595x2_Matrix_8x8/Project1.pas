e1e54c227ba1c4de9c1d44cd522bca6a35baba8e
sechshelme <m_burkhard@gmx.ch>
2018-08-18 21:53:52 +0200

    Atin2313_SPI

--- ATtiny2313/SPI_74HC595x2_Matrix_8x8/Project1.pas
+++ ATtiny2313/SPI_74HC595x2_Matrix_8x8/Project1.pas
@@ -1,17 +1,33 @@
 program Project1;
 
 {$O-}
-
 type
   TSPIGPIO = bitpacked record
-    p0, p1, SlaveSelect, DataOut, DataIn, Clock, p6, p7: boolean;
+    p0, p1, p2, p3, SlaveSelect, DataInt, DataOut, Clock: boolean;
   end;
 
 var
   SPI_Port: TSPIGPIO absolute PORTB;
   SPI_DDR: TSPIGPIO absolute DDRB;
 
+  procedure SPIWriteData(p: PByte; len: byte);
+  var
+    i: byte;
+  begin
+    SPI_Port.SlaveSelect := False;
+    for i := len - 1 downto 0 do begin
+      USIDR := p[i];
+      USISR := 1 shl USIOIF;
 
+      repeat
+        USICR := (%01 shl USIWM) or (%10 shl USICS) or (1 shl USICLK) or (1 shl USITC);
+      until (USISR and (1 shl USIOIF)) <> 0;
+
+    end;
+    SPI_Port.SlaveSelect := True;
+  end;
+
+  // Für Test;
   procedure SPIWriteDataSoft(p: PByte; len: byte);
   var
     i, j: byte;
@@ -28,34 +44,21 @@ var
     SPI_Port.SlaveSelect := True;
   end;
 
-  procedure SPIWriteData(p: PByte; len: byte);
-  var
-    i: byte;
-  begin
-    SPI_Port.SlaveSelect := False;
-    for i := len - 1 downto 0 do begin
-      SPDR := p[i];
-      while (SPSR and (1 shl SPIF)) = 0 do begin
-      end;
-    end;
-    SPI_Port.SlaveSelect := True;
-  end;
-
-
 var
-  z: UInt16 = 0;
+  z: Int16 = 0;
+  i:Int16;
+
 begin
+
   SPI_DDR.DataOut := True;
   SPI_DDR.Clock := True;
   SPI_DDR.SlaveSelect := True;
 
-  SPCR := (1 shl SPE) or (1 shl MSTR) or (%00 shl SPR);
-  SPSR := (1 shl SPI2X);  // SCK x 2 auf 1 MHZ
-
   repeat
+    for i:=0 to 100 do;
     Inc(z);
 
-    //    SPIWriteDataSoft(@z, 2);
     SPIWriteData(@z, 2);
+    //        SPIWriteDataSoft(@z, 2);
   until 1 = 2;
 end.
