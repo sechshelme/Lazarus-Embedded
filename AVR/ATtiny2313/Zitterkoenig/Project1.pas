272d738a6ff5e474632386122c1ecea21fe8d13c
sechshelme <m_burkhard@gmx.ch>
2019-12-26 16:12:59 +0100

    Zitterkönig

--- AVR/ATtiny2313/Zitterkoenig/Project1.pas
+++ AVR/ATtiny2313/Zitterkoenig/Project1.pas
@@ -1,21 +1,13 @@
 program Project1;
 
+uses
+  intrinsics;
+
 {$O-}
 
 const
   WGM01 = 1;
   CS01 = 1;
-
-  procedure sei; assembler;
-  asm
-           Sei
-  end;
-
-  procedure cli; assembler;
-  asm
-           Cli
-  end;
-
 
 const
   digits: packed array[0..17] of byte = (
@@ -49,55 +41,55 @@ var
   SPI_DDR: TSPIGPIO absolute DDRB;
 
   z: Int16 = 0;
-  Data: array[0..3] of byte;
+  Data: array[0..3, 0..2] of byte;
 
 
-  procedure disp_valnnnnn(vval: uint16);
-  var
-    achr: uint8;
-    leer: boolean;
-  begin
-    achr := 0;
-    leer := True;
-    while (vval >= 1000) do begin
-      Dec(vval, 1000);
-      Inc(achr);
-      leer := False;
-    end;
-    if leer then begin
-      achr := 16;
-    end;
-    Data[1] := achr;
-
-    achr := 0;
-    while (vval >= 100) do begin
-      Dec(vval, 100);
-      Inc(achr);
-      leer := False;
-    end;
-    if leer then begin
-      achr := 16;
-    end;
-    Data[1] := achr;
-
-    achr := 0;
-    while (vval >= 10) do begin
-      Dec(vval, 10);
-      Inc(achr);
-      leer := False;
-    end;
-    if leer then begin
-      achr := 16;
-    end;
-    Data[2] := achr;
-
-    achr := 0;
-    while (vval >= 1) do begin
-      Dec(vval);
-      Inc(achr);
-    end;
-    Data[3] := achr;
-  end;
+  //procedure disp_valnnnnn(vval: uint16);
+  //var
+  //  achr: uint8;
+  //  leer: boolean;
+  //begin
+  //  achr := 0;
+  //  leer := True;
+  //  while (vval >= 1000) do begin
+  //    Dec(vval, 1000);
+  //    Inc(achr);
+  //    leer := False;
+  //  end;
+  //  if leer then begin
+  //    achr := 16;
+  //  end;
+  //  Data[1] := achr;
+  //
+  //  achr := 0;
+  //  while (vval >= 100) do begin
+  //    Dec(vval, 100);
+  //    Inc(achr);
+  //    leer := False;
+  //  end;
+  //  if leer then begin
+  //    achr := 16;
+  //  end;
+  //  Data[1] := achr;
+  //
+  //  achr := 0;
+  //  while (vval >= 10) do begin
+  //    Dec(vval, 10);
+  //    Inc(achr);
+  //    leer := False;
+  //  end;
+  //  if leer then begin
+  //    achr := 16;
+  //  end;
+  //  Data[2] := achr;
+  //
+  //  achr := 0;
+  //  while (vval >= 1) do begin
+  //    Dec(vval);
+  //    Inc(achr);
+  //  end;
+  //  Data[3] := achr;
+  //end;
 
 
   procedure SPIWriteData(p: PByte; len: byte);
@@ -118,9 +110,6 @@ var
   end;
 
   procedure Timer0_Interrupt; public Name 'TIMER0_COMPA_ISR'; interrupt;
-  var
-    d: integer;
-    di: byte;
   const
     p: byte = 0;
   begin
@@ -129,66 +118,47 @@ var
       p := 0;
     end;
 
-
-    //    disp_valnnnnn(z);
-
-    //    zahl[1] := d div 100;
-    //    zahl[2] := d mod 100 div 10;
-    //    zahl[3] := d mod 10;
-    SPI_PORT.p0 := False;
-    SPI_PORT.p1 := False;
-    SPI_PORT.p2 := False;
-    SPI_PORT.p3 := False;
-
-    di := digits[p];
-    SPIWriteData(@di, 1);
-    if p = 0 then begin
-      SPI_PORT.p0 := True;
-    end;
-    if p = 1 then begin
-      SPI_PORT.p1 := True;
-    end;
-    if p = 2 then begin
-      SPI_PORT.p2 := True;
-    end;
-    if p = 3 then begin
-      SPI_PORT.p3 := True;
-    end;
+    PORTB := PORTB and %11110000;
+    SPIWriteData(@Data[p], 3);
+    PORTB := PORTB or (1 shl p);
   end;
 
 begin
-  cli();
+  avr_cli();
   SPI_DDR.DataOut := True;
   SPI_DDR.Clock := True;
   SPI_DDR.SlaveSelect := True;
 
-  SPI_DDR.p0 := True;
-  SPI_DDR.p1 := True;
-  SPI_DDR.p2 := True;
-  SPI_DDR.p3 := True;
-
-  Data[0] := 0;
-  Data[1] := 0;
-  Data[2] := 0;
-  Data[3] := 0;
-
-  SPI_PORT.p0 := False;
-  SPI_PORT.p1 := False;
-  SPI_PORT.p2 := False;
-  SPI_PORT.p3 := False;
+  DDRB := DDRB or %00001111;
 
   // -- Timer initialisieren
   TCCR0A := (1 shl WGM01);
   TCCR0B := TCCR0B or %101;
   TIMSK := TIMSK or (1 shl OCIE0A);
-  sei();
-  // --- Ende timer
+  avr_sei();
+  // --- Ende Timer
 
   repeat
+    //    disp_valnnnnn(z);
+
+    //    zahl[1] := d div 100;
+    //    zahl[2] := d mod 100 div 10;
+    //    zahl[3] := d mod 10;
+
     //    Inc(z);
-    Data[0] := digits[1];
-    Data[1] := digits[2];
-    Data[2] := digits[3];
-    Data[3] := digits[4];
+    Data[0, 0] := digits[1];
+    Data[1, 0] := digits[2];
+    Data[2, 0] := digits[3];
+    Data[3, 0] := digits[4];
+
+    Data[0, 1] := digits[3];
+    Data[1, 1] := digits[4];
+    Data[2, 1] := digits[5];
+    Data[3, 1] := digits[6];
+
+    Data[0, 2] := digits[8];
+    Data[1, 2] := digits[7];
+    Data[2, 2] := digits[6];
+    Data[3, 2] := digits[5];
   until 1 = 2;
 end.
