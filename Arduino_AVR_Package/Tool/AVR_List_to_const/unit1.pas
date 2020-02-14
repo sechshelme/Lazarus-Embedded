804654c53558076949308ea654997e24c3c54349
sechshelme <m_burkhard@gmx.ch>
2020-02-14 16:21:13 +0100

    AVR_Package_Tool

--- Arduino_AVR_Package/Tool/AVR_List_to_const/unit1.pas
+++ Arduino_AVR_Package/Tool/AVR_List_to_const/unit1.pas
@@ -40,32 +40,89 @@ end;
 
 procedure TForm1.Button1Click(Sender: TObject);
 var
-  sl: TStringList;
-  SubArchList:String='';
-  s: string;
+  SL_Source, AVR_List: TStringList;
+  SubArchList: string = '';
+  s, s2: string;
   i: integer;
+
 begin
   SynEdit1.Clear;
-  sl := TStringList.Create;
+  SL_Source := TStringList.Create;
+  AVR_List := TStringList.Create;
 
-  sl.LoadFromFile('/home/tux/fpc.src/fpc/rtl/embedded/Makefile');
+  SL_Source.LoadFromFile('/home/tux/fpc.src/fpc/rtl/embedded/Makefile');
 
 
-  for i := 0 to sl.Count - 1 do begin
-    s := sl[i];
+  i := 0;
+  repeat
+    s := SL_Source[i];
     if pos('ifeq ($(SUBARCH),avr', s) > 0 then begin
       Delete(s, 1, 17);
       Delete(s, Length(s), 1);
-      SubArchList+=s+',';
+      SubArchList += s + ',';
+
+      Inc(i);
+      s := SL_Source[i];
+      s2 := s;
+      Delete(s2, 1, 10);
+      if s2[Length(s2)] = '\' then begin
+        Delete(s2, Length(s2), 1);
+      end;
+      s2 := StringReplace(s2, ' ', ',', [rfReplaceAll]);
+      AVR_List.Add('    '#39 + s2 + #39' +');
+
+      while pos('\', s) > 0 do begin
+        Inc(i);
+        s := SL_Source[i];
+        s2 := s;
+        Delete(s2, 1, 3);
+        if s2[Length(s2)] = '\' then begin
+          Delete(s2, Length(s2), 1);
+        end;
+        s2 := StringReplace(s2, ' ', ',', [rfReplaceAll]);
+        AVR_List.Add('    '#39 + s2 + #39' +');
+      end;
+      AVR_List[AVR_List.Count - 1] := StringReplace(AVR_List[AVR_List.Count - 1], ' +', ',', []);
+      AVR_List.Add('');
     end;
-  end;
+    Inc(i);
+  until i >= SL_Source.Count;
+
+  s := AVR_List[AVR_List.Count - 2];
+  Caption := s;
+  Delete(s, Length(s), 1);
+  AVR_List[AVR_List.Count - 2] := s;
+
+  AVR_List.Delete(AVR_List.Count - 1);
   Delete(SubArchList, Length(SubArchList), 1);
+
+  SynEdit1.Lines.Add('');
+  SynEdit1.Lines.Add('// Diese Unit wird automatisch durch das Tool "./Tool/AVR_List_to_const" erzeugt.');
+  SynEdit1.Lines.Add('// Die Arrays werden aus "./fpcsrc/fpc/rtl/embedded/Makefile" importiert.');
+  SynEdit1.Lines.Add('');
+  SynEdit1.Lines.Add('unit');
+  SynEdit1.Lines.Add('  AVR_SubArchList;');
+  SynEdit1.Lines.Add('');
+  SynEdit1.Lines.Add('interface');
+  SynEdit1.Lines.Add('');
   SynEdit1.Lines.Add('const');
   SynEdit1.Lines.Add('  SubArchList = ''' + SubArchList + ''';');
+  SynEdit1.Lines.Add('');
+  SynEdit1.Lines.Add('  AVR_List: array of string = (');
+  for i := 0 to AVR_List.Count - 1 do begin
+    SynEdit1.Lines.Add(AVR_List[i]);
+  end;
+  SynEdit1.Lines.Add(');');
+  SynEdit1.Lines.Add('');
+  SynEdit1.Lines.Add('implementation');
+  SynEdit1.Lines.Add('');
+  SynEdit1.Lines.Add('begin');
+  SynEdit1.Lines.Add('end.');
 
-  sl.Free;
+  SynEdit1.Lines.SaveToFile('../../avr_subarchList.pas');
+
+  AVR_List.Free;
+  SL_Source.Free;
 end;
 
 end.
-
-
---
+++ Arduino_AVR_Package/avr_subarchList.pas
@@ -0,0 +1,62 @@
+
+// Diese Unit wird automatisch durch das Tool "./Tool/AVR_List_to_const" erzeugt.
+// Die Arrays werden aus "./fpcsrc/fpc/rtl/embedded/Makefile" importiert.
+
+unit
+  AVR_SubArchList;
+
+interface
+
+const
+  SubArchList = 'avrtiny,avr1,avr2,avr25,avr35,avr4,avr5,avr51,avr6,avrxmega3';
+
+  AVR_List: array of string = (
+    'attiny4,attiny5,attiny9,attiny10,attiny20,attiny40,attiny102,attiny104',
+
+    'attiny11,attiny12,attiny15,attiny28',
+
+    'attiny26',
+
+    'attiny13,attiny13a,attiny24,attiny24a,attiny25,attiny43u,' +
+    'attiny44,attiny44a,attiny45,attiny48,attiny84a,attiny84,attiny85,' +
+    'attiny87,attiny88,attiny261,attiny261a,attiny441,attiny461,attiny461a,attiny828,' +
+    'attiny841,attiny861,attiny861a,attiny2313,attiny2313a,attiny4313',
+
+    'at90usb82,at90usb162,atmega8u2,atmega16u2,atmega32u2,attiny167,attiny1634',
+
+    'at90pwm1,at90pwm2b,at90pwm3b,at90pwm81,ata6285,ata6286,atmega8,' +
+    'atmega8a,atmega8hva,atmega48,atmega48a,atmega48p,atmega48pa,' +
+    'atmega48pb,atmega88,atmega88a,atmega88p,atmega88pa,atmega88pb,' +
+    'atmega8515,atmega8535',
+
+    'avrsim,at90can32,at90can64,at90pwm161,at90pwm216,at90pwm316,' +
+    'at90usb646,at90usb647,atmega16,atmega16a,atmega16hva,atmega16hvb,atmega16hvbrevb,atmega16m1,' +
+    'atmega16u4,atmega32,atmega32a,atmega32c1,atmega32hvb,atmega32hvbrevb,atmega32m1,' +
+    'atmega32u4,atmega64,atmega64a,atmega64c1,atmega64hve2,atmega64m1,atmega64rfr2,atmega162,' +
+    'atmega164a,atmega164p,atmega164pa,atmega165a,atmega165p,atmega165pa,' +
+    'atmega168,atmega168a,atmega168p,atmega168pa,atmega168pb,atmega169a,atmega169p,' +
+    'atmega169pa,atmega324a,atmega324p,atmega324pa,atmega324pb,atmega325,atmega325a,' +
+    'atmega325p,atmega325pa,atmega328,atmega328p,atmega328pb,atmega329,atmega329a,' +
+    'atmega329p,atmega329pa,atmega406,atmega640,atmega644,atmega644a,atmega644p,' +
+    'atmega644pa,atmega644rfr2,atmega645,atmega645a,atmega645p,atmega649,atmega649a,' +
+    'atmega649p,atmega3250,atmega3250a,atmega3250p,atmega3250pa,atmega3290,' +
+    'atmega3290a,atmega3290p,atmega3290pa,atmega6450,atmega6450a,' +
+    'atmega6450p,atmega6490,atmega6490a,atmega6490p',
+
+    'at90can128,at90usb1286,at90usb1287,atmega128,atmega128a,atmega128rfa1,atmega128rfr2,' +
+    'atmega1280,atmega1281,atmega1284,atmega1284p,atmega1284rfr2',
+
+    'avrsim,atmega256rfr2,atmega2560,atmega2561,atmega2564rfr2',
+
+    'atmega808,atmega809,atmega1608,atmega1609,atmega3208,atmega3209,' +
+    'atmega4808,atmega4809,attiny202,attiny204,attiny212,attiny214,' +
+    'attiny402,attiny404,attiny406,attiny412,attiny414,attiny416,' +
+    'attiny416auto,attiny417,attiny804,attiny806,attiny807,attiny814,' +
+    'attiny816,attiny817,attiny1604,attiny1606,attiny1607,attiny1614,' +
+    'attiny1616,attiny1617,attiny1624,attiny1626,attiny1627,attiny3214,attiny3216,attiny3217'
+);
+
+implementation
+
+begin
+end.
