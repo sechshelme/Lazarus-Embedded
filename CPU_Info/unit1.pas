307c177db34d7dfbcdab23e6fe9ba67e2c35fb28
sechshelme <m_burkhard@gmx.ch>
2020-03-22 16:02:15 +0100

    Embedded_GUI_AVR_CpuInfo

--- CPU_Info/unit1.pas
+++ CPU_Info/unit1.pas
@@ -19,6 +19,10 @@ type
     procedure Button1Click(Sender: TObject);
     procedure ComboBox1Select(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure StringGrid1CompareCells(Sender: TObject;
+      ACol, ARow, BCol, BRow: integer; var Result: integer);
+    procedure StringGrid1HeaderClick(Sender: TObject; IsColumn: boolean;
+      Index: integer);
   private
 
   public
@@ -65,6 +69,42 @@ begin
   Load(AVRControllerDataList);
 end;
 
+procedure TForm1.StringGrid1CompareCells(Sender: TObject;
+  ACol, ARow, BCol, BRow: integer; var Result: integer);
+
+  function IsNumber(s: string): boolean;
+  var
+    i, er: integer;
+  begin
+    Val(s, i, er);
+    Result := er = 0;
+  end;
+
+begin
+  IsNumber('a');
+
+  Result := CompareStr(StringGrid1.Cells[ACol, ARow], StringGrid1.Cells[BCol, BRow]);
+  if Result = 0 then begin
+    Result := CompareStr(StringGrid1.Cells[2, ARow], StringGrid1.Cells[2, BRow]);
+    if Result = 0 then begin
+      Result := CompareStr(StringGrid1.Cells[0, ARow], StringGrid1.Cells[0, BRow]);
+    end;
+  end;
+  //if Result = 0 then begin
+  //  if ACol = 0 then begin
+  //    Result := CompareStr(StringGrid1.Cells[1, ARow], StringGrid1.Cells[1, BRow]);
+  //  end else begin
+  //    Result := CompareStr(StringGrid1.Cells[0, ARow], StringGrid1.Cells[0, BRow]);
+  //  end;
+  //end;
+end;
+
+procedure TForm1.StringGrid1HeaderClick(Sender: TObject; IsColumn: boolean;
+  Index: integer);
+begin
+  StringGrid1.SortColRow(True, Index, 1, StringGrid1.RowCount - 1);
+end;
+
 procedure TForm1.Load(Table: array of TStringArray);
 var
   x, y: integer;
@@ -77,8 +117,8 @@ begin
     end;
   end;
   StringGrid1.AutoSizeColumns;
-  StringGrid1.SortColRow(True, 0, 1, StringGrid1.RowCount-1);
-  StringGrid1.SortColRow(True, 2, 1, StringGrid1.RowCount-1);
+  //  StringGrid1.SortColRow(True, 0, 1, StringGrid1.RowCount-1);
+  //  StringGrid1.SortColRow(True, 2, 1, StringGrid1.RowCount - 1);
 end;
 
 end.
