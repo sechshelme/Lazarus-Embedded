6b8bf2ab3652bdb472459ac50a9b0eafa5b2e00a
sechshelme <m_burkhard@gmx.ch>
2020-05-24 16:10:08 +0200

    xml

---
+++ test/XML/unit1.pas
@@ -0,0 +1,153 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComCtrls,
+  XMLConf,
+  XMLRead, XMLWrite, DOM;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Memo1: TMemo;
+    TreeView1: TTreeView;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+  private
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+const
+  Options_File = 'config.xml';
+  FormPos = '/FormPos/';
+
+procedure LoadFormPos_from_XML(Form: TControl);
+var
+  Cfg: TXMLConfig;
+begin
+  Cfg := TXMLConfig.Create(nil);
+  Cfg.Filename := Options_File;
+  Form.Left := Cfg.GetValue(Form.Name + FormPos + 'Left', Form.Left);
+  Form.Top := Cfg.GetValue(Form.Name + FormPos + 'Top', Form.Top);
+  Form.Width := Cfg.GetValue(Form.Name + FormPos + 'Width', Form.Width);
+  Form.Height := Cfg.GetValue(Form.Name + FormPos + 'Height', Form.Height);
+  Cfg.Free;
+end;
+
+procedure SaveFormPos_to_XML(Form: TControl);
+var
+  Cfg: TXMLConfig;
+begin
+  Cfg := TXMLConfig.Create(nil);
+  Cfg.Filename := Options_File;
+  Cfg.SetValue(Form.Name + FormPos + 'Left', Form.Left);
+  Cfg.SetValue(Form.Name + FormPos + 'Top', Form.Top);
+  Cfg.SetValue(Form.Name + FormPos + 'Width', Form.Width);
+  Cfg.SetValue(Form.Name + FormPos + 'Height', Form.Height);
+  Cfg.Free;
+end;
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+const
+//  path = 'test.xml';
+  path = 'XML/attiny4.xml';
+var
+  doc: TXMLDocument;
+  poly_Node: TDOMNode;
+
+  nodeList: TDOMNodeList;
+
+  procedure Node(n: TDOMNodeList; t: TTreeNode; schachtel: integer);
+  var
+    i, j: integer;
+  begin
+    for i := 0 to n.Count - 1 do begin
+      if n.Item[i].NodeName = '#text' then begin
+        Memo1.Lines.Add(StringOfChar(' ', schachtel * 4) + n.Item[i].NodeValue);
+        TreeView1.Items.AddChild(t, n.Item[i].NodeValue);
+      end else begin
+        Memo1.Lines.Add(StringOfChar(' ', schachtel * 4) + n.Item[i].NodeName);
+        TreeView1.Items.AddChild(t, n.Item[i].NodeName);
+      end;
+
+      if n.Item[i].HasAttributes then begin
+        for j := 0 to n.Item[i].Attributes.Length - 1 do begin
+          Memo1.Lines.Add(StringOfChar(' ', schachtel * 4 + 4) + n.Item[i].Attributes.Item[j].NodeName + ': ' + n.Item[i].Attributes.Item[j].NodeValue);
+          TreeView1.Items.AddChild(t.Items[i], n.Item[i].Attributes.Item[j].NodeName + ': ' + n.Item[i].Attributes.Item[j].NodeValue);
+        end;
+      end;
+
+      if n.Item[i].HasChildNodes then begin
+        //        TreeView1.Items.AddChild(t,'bla bla');
+        Node(n.Item[i].ChildNodes, t.Items[i], schachtel + 1);
+      end;
+
+    end;
+  end;
+
+begin
+  Memo1.Clear;
+  TreeView1.Items.Clear;
+  ReadXMLFile(doc, path);
+
+  //  poly_Node := doc.DocumentElement.FindNode('test');
+
+  //  Memo1.Lines.Add(poly_Node.FirstChild.NodeValue);
+  //  Memo1.Lines.Add(poly_Node.TextContent);
+
+  //  nodeList := doc.DocumentElement.ChildNodes;
+
+
+  nodeList := doc.DocumentElement.ChildNodes;
+  TreeView1.Items.Add(nil, 'abc');
+  Node(nodeList, TreeView1.Items[0], 0);
+
+  //with nodeList do begin
+  //  ct := Count;
+  //  Memo1.Lines.Add(ct.ToString);
+  //end;
+  //
+  doc.Free;
+
+  //Doc := TXMLDocument.Create;
+  //WriteXMLFile(doc, path);
+  //doc.Free;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var
+  t: TTreeNode;
+begin
+  TreeView1.Items.Add(nil, 'abc');
+
+  TreeView1.Items.AddChild(TreeView1.Items[0], '123');
+  TreeView1.Items.AddChild(nil, '456');
+  TreeView1.Items.AddChild(TreeView1.Items[0].Items[0], '789');
+  TreeView1.Items.Add(nil, 'abc');
+
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  SaveFormPos_to_XML(self);
+end;
+
+end.
