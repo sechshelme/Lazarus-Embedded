4996dd570f5de56a6e40e331e2a7d8209bc6d579
sechshelme <m_burkhard@gmx.ch>
2020-07-22 14:46:13 +0200

    fuses_const

--- Tools/AVR_XML_to_Fuse_Const/embedded_gui_avr_fuse_form.pas
+++ Tools/AVR_XML_to_Fuse_Const/embedded_gui_avr_fuse_form.pas
@@ -6,13 +6,9 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComCtrls,
-  ExtCtrls, FileUtil, LazFileUtils, Laz2_XMLCfg, laz2_XMLRead, laz2_XMLWrite, laz2_DOM,
+  ExtCtrls, FileUtil, LazFileUtils, laz2_XMLRead, laz2_DOM,
   Embedded_GUI_Common,
-  Embedded_GUI_Run_Command,
-  Embedded_GUI_AVR_Fuse_Common,
-
-  //   Embedded_GUI_AVR_Fuse_Const,
-
+  Embedded_GUI_AVR_Fuse_Const,
   Embedded_GUI_AVR_Fuse_TabSheet;
 
 type
@@ -21,25 +17,16 @@ type
 
   TForm_AVR_Fuse = class(TForm)
     Button_CreateConst: TButton;
-    Button_ReadFuse: TButton;
     Button_Close: TButton;
-    ComboBox1: TComboBox;
     Label1: TLabel;
-    PageControl1: TPageControl;
     procedure Button_CloseClick(Sender: TObject);
     procedure Button_CreateConstClick(Sender: TObject);
-    procedure Button_ReadFuseClick(Sender: TObject);
-    procedure ComboBox1Change(Sender: TObject);
-    procedure CreateTab(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
   private
     function IsAttribut(Node: TDOMNode; const NodeName, NodeValue: string): boolean;
     function GetAttribut(Node: TDOMNode; const NodeName: string): string;
-    procedure Read_Value_Group(const Attr_name: string; Node: TDOMNode; TabSheet: TFuseTabSheet);
-    procedure ClearTabs;
   private
-    FuseTabSheet: array of TFuseTabSheet;
   public
   end;
 
@@ -84,66 +71,12 @@ begin
   end;
 end;
 
-procedure TForm_AVR_Fuse.ClearTabs;
-var
-  i: integer;
-begin
-  for i := 0 to Length(FuseTabSheet) - 1 do begin
-    FuseTabSheet[i].Free;
-  end;
-  SetLength(FuseTabSheet, 0);
-end;
-
-procedure TForm_AVR_Fuse.Read_Value_Group(const Attr_name: string; Node: TDOMNode; TabSheet: TFuseTabSheet);
-var
-  Node_Value_Group, Node_Value: TDOMNode;
-  s: string;
-begin
-  Node_Value_Group := Node.FirstChild;
-  while Node_Value_Group <> nil do begin
-    if IsAttribut(Node_Value_Group, 'name', Attr_name) then begin
-      Node_Value := Node_Value_Group.FirstChild;
-      while Node_Value <> nil do begin
-        s := GetAttribut(Node_Value, 'caption');
-        s += ' (' + GetAttribut(Node_Value, 'name') + ')';
-        TabSheet.AddComboxItem(s, GetAttribut(Node_Value, 'value').ToInteger);
-        Node_Value := Node_Value.NextSibling;
-      end;
-    end;
-    Node_Value_Group := Node_Value_Group.NextSibling;
-  end;
-
-end;
-
 /// --- public
 
 procedure TForm_AVR_Fuse.FormCreate(Sender: TObject);
-var
-  fl: TStringList;
-  i: integer;
 begin
-  Caption := Title + 'AVR Fuse';
-  ComboBox1.Sorted := True;
-  ComboBox1.Style := csOwnerDrawFixed;
-
-  fl := FindAllFiles('../AVR_Fuse/XML', '*.XML', False);
-  for i := 0 to fl.Count - 1 do begin
-    ComboBox1.Items.Add(fl[i]);
-  end;
-  fl.Free;
-
+  Caption := Title + 'Create AVR Fuse Const';
   LoadFormPos_from_XML(self);
-end;
-
-procedure TForm_AVR_Fuse.ComboBox1Change(Sender: TObject);
-begin
-  //  AVR_XML_Path := ComboBox1.Items[ComboBox1.ItemIndex];
-  AVR_XML_Path := ComboBox1.Text;
-  Caption := AVR_XML_Path;
-  if FileExists(AVR_XML_Path) then begin
-    ClearTabs;
-    CreateTab(Sender);
-  end;
 end;
 
 procedure TForm_AVR_Fuse.Button_CloseClick(Sender: TObject);
@@ -151,177 +84,45 @@ begin
   Close;
 end;
 
-procedure TForm_AVR_Fuse.CreateTab(Sender: TObject);
-var
-  l: integer;
-  doc: TXMLDocument;
-  Node_Modules, Node_Module, Node_Register_group, Node_Register, Node_Bitfield: TDOMNode;
-
-  procedure AddFuse(Node_Register: TDOMNode);
-  var
-    n: string;
-    ofs: byte;
-    i: integer;
-  begin
-    i := Length(FuseTabSheet);
-    SetLength(FuseTabSheet, i + 1);
-    FuseTabSheet[i] := TFuseTabSheet.Create(Self);
-    with FuseTabSheet[i] do begin
-      Tag := i;
-      PageControl := PageControl1;
-      ofs := StrToInt(GetAttribut(Node_Register, 'offset'));
-      n := GetAttribut(Node_Register, 'name');
-      case n of
-        'EXTENDED': begin
-          FuseName := 'efuse';
-        end;
-        'HIGH': begin
-          FuseName := 'hfuse';
-        end;
-        'LOW': begin
-          FuseName := 'lfuse';
-        end;
-        'LOCKBIT': begin
-          FuseName := 'lock';
-        end;
-        'BYTE0': begin
-          FuseName := 'BYTE0';
-        end else begin
-          FuseName := 'fuse' + IntToStr(ofs);
-        end;
-      end;
-      Caption := n + ' (' + FuseName + ')';
-    end;
-  end;
-
-begin
-  ReadXMLFile(doc, AVR_XML_Path);
-
-  Node_Modules := doc.DocumentElement.FindNode('modules');
-  if Node_Modules <> nil then begin
-
-    Node_Module := Node_Modules.FirstChild;
-    while Node_Module <> nil do begin
-      if Node_Module.NodeName = 'module' then begin
-        if (IsAttribut(Node_Module, 'name', 'FUSE')) or (IsAttribut(Node_Module, 'name', 'LOCKBIT')) then begin
-
-          Node_Register_group := Node_Module.FirstChild;
-          if Node_Register_group <> nil then begin
-            Node_Register := Node_Register_group.FirstChild;
-            while Node_Register <> nil do begin
-
-              AddFuse(Node_Register);
-
-              Node_Bitfield := Node_Register.FirstChild;
-              while Node_Bitfield <> nil do begin
-
-                l := Length(FuseTabSheet) - 1;
-                if GetAttribut(Node_Bitfield, 'values') <> '' then begin
-                  FuseTabSheet[l].NewComboBox(GetAttribut(Node_Bitfield, 'caption') + ' (' + GetAttribut(Node_Bitfield, 'name') + '):',
-                    StrToInt(GetAttribut(Node_Bitfield, 'mask')));
-                  Read_Value_Group(GetAttribut(Node_Bitfield, 'values'), Node_Module, FuseTabSheet[l]);
-                end else begin
-                  FuseTabSheet[l].AddCheckBox(GetAttribut(Node_Bitfield, 'caption') + ' (' + GetAttribut(Node_Bitfield, 'name') + ')',
-                    StrToInt(GetAttribut(Node_Bitfield, 'mask')));
-                end;
-
-                Node_Bitfield := Node_Bitfield.NextSibling;
-              end;
-              Node_Register := Node_Register.NextSibling;
-            end;
-          end;
-        end;
-      end;
-      Node_Module := Node_Module.NextSibling;
-    end;
-  end;
-  if PageControl1.PageCount > 0 then begin
-    PageControl1.TabIndex := 0;
-  end;
-
-  doc.Free;
-end;
-
-procedure TForm_AVR_Fuse.Button_ReadFuseClick(Sender: TObject);
-var
-  avr, fuse: string;
-  i, l, j: integer;
-begin
-  if not Assigned(Run_Command_Form) then begin
-    Run_Command_Form := TRun_Command_Form.Create(nil);
-  end;
-  Run_Command_Form.Memo1.Clear;
-
-  avr := ExtractFileName(AVR_XML_Path);
-  avr := ExtractFileNameWithoutExt(avr);
-  //  WriteLn(avr);
-
-  l := Length(FuseTabSheet);
-  for i := 0 to l - 1 do begin
-    fuse := ' -U' + FuseTabSheet[i].FuseName + ':r:-:h';
-    Run_Command_Form.RunCommand('avrdude -cusbasp -p' + avr + fuse);
-    if Run_Command_Form.ExitCode = 0 then begin
-      j := Run_Command_Form.Memo1.Lines.Count - 1;
-      while j >= 0 do begin
-        if Pos('0x', Run_Command_Form.Memo1.Lines[j]) = 1 then begin
-          FuseTabSheet[i].FuseByte := not StrToInt(Run_Command_Form.Memo1.Lines[j]);
-          j := 0;
-        end;
-        Dec(j);
-      end;
-    end;
-  end;
-end;
-
 procedure TForm_AVR_Fuse.Button_CreateConstClick(Sender: TObject);
 var
   fl, sl: TStringList;
   ii: integer;
-  l: integer;
   s: string;
   doc: TXMLDocument;
   Node_Modules, Node_Module, Node_Register_group, Node_Register, Node_Bitfield: TDOMNode;
 
   procedure AddFuse(Node_Register: TDOMNode);
   var
-    n: string;
+    n, FuseName: string;
     ofs: byte;
-    i: integer;
   begin
-    i := Length(FuseTabSheet);
-    SetLength(FuseTabSheet, i + 1);
-    FuseTabSheet[i] := TFuseTabSheet.Create(Self);
-    with FuseTabSheet[i] do begin
-      Tag := i;
-      PageControl := PageControl1;
-      ofs := StrToInt(GetAttribut(Node_Register, 'offset'));
-      n := GetAttribut(Node_Register, 'name');
-      case n of
-        'EXTENDED': begin
-          FuseName := 'efuse';
-        end;
-        'HIGH': begin
-          FuseName := 'hfuse';
-        end;
-        'LOW': begin
-          FuseName := 'lfuse';
-        end;
-        'LOCKBIT': begin
-          FuseName := 'lock';
-        end;
-        'BYTE0': begin
-          FuseName := 'BYTE0';
-        end else begin
-          FuseName := 'fuse' + IntToStr(ofs);
-        end;
+    ofs := StrToInt(GetAttribut(Node_Register, 'offset'));
+    n := GetAttribut(Node_Register, 'name');
+    case n of
+      'EXTENDED': begin
+        FuseName := 'efuse';
       end;
-      Caption := n + ' (' + FuseName + ')';
-      sl.Add('      (Caption: '#39 + n + #39'; FuseName: '#39 + FuseName + #39'; ofs: $' + IntToHex(ofs, 2) + '; BitField:(');
-
+      'HIGH': begin
+        FuseName := 'hfuse';
+      end;
+      'LOW': begin
+        FuseName := 'lfuse';
+      end;
+      'LOCKBIT': begin
+        FuseName := 'lock';
+      end;
+      'BYTE0': begin
+        FuseName := 'BYTE0';
+      end else begin
+        FuseName := 'fuse' + IntToStr(ofs);
+      end;
     end;
+    Caption := n + ' (' + FuseName + ')';
+    sl.Add('      (Caption: '#39 + n + #39'; Name: '#39 + FuseName + #39'; ofs: $' + IntToHex(ofs, 2) + '; BitField:(');
   end;
 
-  procedure Read_Value_Group2(const Attr_name: string; Node: TDOMNode; TabSheet: TFuseTabSheet);
+  procedure Read_Value_Group2(const Attr_name: string; Node: TDOMNode);
   var
     Node_Value_Group, Node_Value: TDOMNode;
     s: string;
@@ -333,8 +134,6 @@ var
         while Node_Value <> nil do begin
           s := GetAttribut(Node_Value, 'caption');
           s += ' (' + GetAttribut(Node_Value, 'name') + ')';
-          TabSheet.AddComboxItem(s, GetAttribut(Node_Value, 'value').ToInteger);
-
           sl.Add('           (Caption: '#39 + GetAttribut(Node_Value, 'caption') + #39'; Name: '#39 + GetAttribut(Node_Value, 'name') + #39'; Value: $' + IntToHex(StrToInt(GetAttribut(Node_Value, 'value')), 2) + '),');
 
           Node_Value := Node_Value.NextSibling;
@@ -360,7 +159,7 @@ begin
   sl.Add('  TAVR_Fuse_Data = array of record');
   sl.Add('    Name: String;');
   sl.Add('    Fuses: array of record');
-  sl.Add('      Caption, FuseName: String;');
+  sl.Add('      Caption, Name: String;');
   sl.Add('      ofs: Byte;');
   sl.Add('      BitField: array of record');
   sl.Add('        Caption, Name: String;');
@@ -377,12 +176,13 @@ begin
   sl.Add('  AVR_Fuse_Data: TAVR_Fuse_Data = (');
 
   fl := FindAllFiles('../AVR_Fuse/XML', '*.XML', False);
-  for ii := 0 to 13 do begin
-    //    for ii := 0 to fl.Count - 1 do begin
+  for ii := 0 to fl.Count - 1 do begin
     ReadXMLFile(doc, fl[ii]);
     s := ExtractFileName(fl[ii]);
     s := ExtractFileNameWithoutExt(s);
 
+    sl.Add('');
+    sl.Add('// ---------- ' + s + '-----------');
     sl.Add('    (Name: '#39 + s + #39'; Fuses:(');
 
     Node_Modules := doc.DocumentElement.FindNode('modules');
@@ -396,7 +196,6 @@ begin
             Node_Register_group := Node_Module.FirstChild;
             if Node_Register_group <> nil then begin
               Node_Register := Node_Register_group.FirstChild;
-              //              if Node_Register=nil then;  sl.Add(')),');
               while Node_Register <> nil do begin
 
                 AddFuse(Node_Register);
@@ -404,16 +203,10 @@ begin
                 Node_Bitfield := Node_Register.FirstChild;
                 while Node_Bitfield <> nil do begin
 
-                  l := Length(FuseTabSheet) - 1;
                   if GetAttribut(Node_Bitfield, 'values') <> '' then begin
-                    FuseTabSheet[l].NewComboBox(GetAttribut(Node_Bitfield, 'caption') + ' (' + GetAttribut(Node_Bitfield, 'name') + '):',
-                      StrToInt(GetAttribut(Node_Bitfield, 'mask')));
-
                     sl.Add('         (Caption: '#39 + GetAttribut(Node_Bitfield, 'caption') + #39'; ' + 'Name: '#39 + GetAttribut(Node_Bitfield, 'name') + #39'; ' + 'Mask: $' + IntToHex(StrToInt(GetAttribut(Node_Bitfield, 'mask')), 2) + ';' + ' Values: (');
-                    Read_Value_Group2(GetAttribut(Node_Bitfield, 'values'), Node_Module, FuseTabSheet[l]);
+                    Read_Value_Group2(GetAttribut(Node_Bitfield, 'values'), Node_Module);
                   end else begin
-                    FuseTabSheet[l].AddCheckBox(GetAttribut(Node_Bitfield, 'caption') + ' (' + GetAttribut(Node_Bitfield, 'name') + ')',
-                      StrToInt(GetAttribut(Node_Bitfield, 'mask')));
                     sl.Add('         (Caption: '#39 + GetAttribut(Node_Bitfield, 'caption') + #39'; ' + 'Name: '#39 + GetAttribut(Node_Bitfield, 'name') + #39'; ' + 'Mask: $' + IntToHex(StrToInt(GetAttribut(Node_Bitfield, 'mask')), 2) + ';' + ' Values: ()),');
                   end;
 
@@ -423,7 +216,7 @@ begin
                 if Pos('),', sl[sl.Count - 1]) > 0 then begin
                   sl[sl.Count - 1] := StringReplace(sl[sl.Count - 1], '),', '))),', []);
                 end else begin
-                  sl[sl.Count - 1]:=sl[sl.Count - 1]+')),';
+                  sl[sl.Count - 1] := sl[sl.Count - 1] + ')),';
                 end;
 
                 Node_Register := Node_Register.NextSibling;
@@ -433,9 +226,6 @@ begin
         end;
         Node_Module := Node_Module.NextSibling;
       end;
-    end;
-    if PageControl1.PageCount > 0 then begin
-      PageControl1.TabIndex := 0;
     end;
 
     doc.Free;
@@ -457,14 +247,8 @@ begin
 end;
 
 procedure TForm_AVR_Fuse.FormDestroy(Sender: TObject);
-var
-  i: integer;
 begin
   SaveFormPos_to_XML(self);
-  for i := 0 to Length(FuseTabSheet) - 1 do begin
-    FuseTabSheet[i].Free;
-  end;
-  SetLength(FuseTabSheet, 0);
 end;
 
 end.
