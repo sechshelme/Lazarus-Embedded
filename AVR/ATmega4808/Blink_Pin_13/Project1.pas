826e86ddbf92520a4dfbdbc4b1d03bb4b25ff364
sechshelme <m_burkhard@gmx.ch>
2020-08-03 17:00:59 +0200

    4808

--- AVR/ATmega4808/Blink_Pin_13/Project1.pas
+++ AVR/ATmega4808/Blink_Pin_13/Project1.pas
@@ -5,31 +5,48 @@ program Project1;
 uses
   intrinsics;
 
-procedure Delay;
-var
-  i:Int16;
-begin
-   for i:=0 to 30000 do;
-end;
+  procedure Delay;
+  var
+    i: Int16;
+  begin
+    for i := 0 to 30000 do begin
+    end;
+  end;
 
 const
-  pin2=1 shl 2;
-
+  pin0 = 1 shl 0;
+  pin1 = 1 shl 1;
+  pin2 = 1 shl 2;
+  pin3 = 1 shl 3;
+  pin4 = 1 shl 4;
+  pin5 = 1 shl 5;
+  pin6 = 1 shl 6;
+  pin7 = 1 shl 7;
 
 begin
   // Setup
 
-  PORTC.DIRSET:=pin2;
+  PORTC.DIRSET := %11111111;
+  PORTF.DIRSET := pin5;
 
   repeat
-    // Loop;
-//    PORTC.OUTTGL:=pin2;
-//    Delay;
+    // Schaltet Pin um
+    PORTC.OUTTGL := pin2;
+    PORTC.OUTTGL := pin3;
+    PORTF.OUTTGL := pin5;
 
-    PORTC.OUTSET:=pin2;
+    // Klassisch
+    PORTC.OUT_ := PORTC.OUT_ or pin1;
     Delay;
-    PORTC.OUTCLR:=pin2;
+    PORTC.OUT_ := PORTC.OUT_ and not pin1;
     Delay;
 
-  until false;
+    // Setzt Pin
+    PORTC.OUTSET := pin0;
+    Delay;
+    // Löscht Pin
+    PORTC.OUTCLR := pin0;
+    Delay;
+
+  until False;
 end.
