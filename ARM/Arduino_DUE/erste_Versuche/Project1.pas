6a3fa61c1931afeb47479970bee6022e53b721c8
sechshelme <m_burkhard@gmx.ch>
2020-08-05 15:01:38 +0200

    arduino_due

---
+++ ARM/Arduino_DUE/erste_Versuche/Project1.pas
@@ -0,0 +1,15 @@
+program Project1;
+
+{$H-,J-,O-}
+
+uses
+  cortexm3;
+
+begin
+  // Setup
+  repeat
+
+//    USART3_IRQn;
+    // Loop;
+  until false;
+end.
--- test/RunCommand/unit1.pas
+++ test/RunCommand/unit1.pas
@@ -14,12 +14,13 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
-    Button2: TButton;
+    Button_Reset: TButton;
     com: TButton;
     avr: TButton;
     Memo1: TMemo;
     procedure avrClick(Sender: TObject);
     procedure Button1Click(Sender: TObject);
+    procedure Button_ResetClick(Sender: TObject);
     procedure comClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
   private
@@ -48,6 +49,22 @@ begin
     Memo1.Lines.Add('geklappt ! ');
     Memo1.Lines.Add(s);
   end;
+end;
+
+procedure TForm1.Button_ResetClick(Sender: TObject);
+var
+  SerialHandle: TSerialHandle;
+begin
+  SerialHandle := SerOpen('/dev/ttyACM0');
+  SerSetParams(SerialHandle, 1200, 8, NoneParity, 1, []);
+
+  SerSetDTR(SerialHandle, True);
+  SerSetDTR(SerialHandle, False);
+  Sleep(500);
+  SerClose(SerialHandle);
+  Sleep(500);
+
+  Caption := 'reset';
 end;
 
 procedure TForm1.comClick(Sender: TObject);
@@ -79,6 +96,10 @@ end;
 procedure TForm1.avrClick(Sender: TObject);
 var
   SerialHandle: TSerialHandle;
+const
+  bos='/home/tux/.arduino15/packages/arduino/tools/bossac/1.6.1-arduino/bossac';
+  bin0 = '/tmp/arduino_build_798287/Blink.ino.bin';
+  bin1 = '/n4800/DATEN/Programmierung/Lazarus/Tutorials/Embedded/ARM/Arduino_DUE/erste_Versuche/Project1.bin';
 begin
   if not Assigned(RunCommandForm) then begin
     RunCommandForm := TRun_Command_Form.Create(nil);
@@ -94,8 +115,19 @@ begin
   Sleep(500);
 
 
-//  RunCommandForm.RunCommand('avrdude -patmega32u4 -v -cavr109 -P/dev/ttyACM0 -b57600 -D -Uflash:w:/home/tux/Dropbox/Sloeber_Projecte/sloeber-workspace/leonardo_test/Release/leonardo_test.hex:i');
-  RunCommandForm.RunCommand('/home/tux/.arduino15/packages/arduino/tools/bossac/1.6.1-arduino/bossac -i -d --port=ttyACM0 -U false -e -w -v -b /tmp/arduino_build_790238/Blink.ino.bin -R');
+  //  RunCommandForm.RunCommand('avrdude -patmega32u4 -v -cavr109 -P/dev/ttyACM0 -b57600 -D -Uflash:w:/home/tux/Dropbox/Sloeber_Projecte/sloeber-workspace/leonardo_test/Release/leonardo_test.hex:i');
+  //RunCommandForm.RunCommand('/home/tux/.arduino15/packages/arduino/tools/bossac/1.6.1-arduino/bossac -i -d --port=ttyACM0 -U false -e -w -v -b /tmp/arduino_build_798287/Blink.ino.bin -R');
+//  RunCommandForm.RunCommand(bos +' -i -d --port=ttyACM0 -U false -e -w -v -b ' + bin0 + ' -R');
+//RunCommandForm.RunCommand(bos +' -i -d --port=ttyACM0 -U false -e -w -v -b ' + bin1 + ' -R');
+RunCommandForm.RunCommand(bos +' -i -d --port=ttyACM0 -e -w -v -b ' + bin1 + '');
+
+// Programmming
+// /home/tux/.arduino15/packages/arduino/tools/bossac/1.6.1-arduino/bossac -i -d --port=ttyACM0 -U false -e -w -v -b /tmp/arduino_build_798287/Blink.ino.bin -R
+
+// Native
+// /home/tux/.arduino15/packages/arduino/tools/bossac/1.6.1-arduino/bossac -i -d --port=ttyACM0 -U true -e -w -v -b /tmp/arduino_build_798287/Blink.ino.bin -R
+
+
   Caption := RunCommandForm.ExitCode.ToString;
 end;
 
