d3eb3f7c032f0eae228ce3c91861da5892a4a1f2
sechshelme <m_burkhard@gmx.ch>
2022-04-12 17:22:36 +0200

    findpara

--- Versuche_fuer_Package/FindPara_Test/unit1.pas
+++ Versuche_fuer_Package/FindPara_Test/unit1.pas
@@ -30,27 +30,37 @@ implementation
 
 {$R *.lfm}
 
-function FindPara(const Source: string; Sub: string; FirstSpace: boolean = True): string;
+function FindPara(const Source: string; Sub: TStringArray; FirstSpace: boolean = True): string;
 var
-  p, Index: integer;
+  p, i, Index: integer;
 begin
-  if FirstSpace then begin
-    sub := ' ' + Sub;
-  end;
-  p := pos(Sub, Source);
-  while Copy(Source, p + Length(Sub), 1) = ' ' do begin
-    Inc(p);
-  end;
   Result := '';
-  if p > 0 then begin
-    p += Length(Sub);
-    Index := p;
-    while (Index <= Length(Source)) and (Source[Index] > #32) do begin
-      Result += Source[Index];
-      Inc(Index);
+  i := 0;
+  while (i < Length(Sub)) and (Result = '') do begin
+
+    if FirstSpace then begin
+      sub[i] := ' ' + Sub[i];
     end;
+    p := pos(Sub[i], Source);
+    while Copy(Source, p + Length(Sub[i]), 1) = ' ' do begin
+      Inc(p);
+    end;
+    if p > 0 then begin
+      p += Length(Sub[i]);
+      Index := p;
+      while (Index <= Length(Source)) and (Source[Index] > #32) do begin
+        Result += Source[Index];
+        Inc(Index);
+      end;
+    end;
+    Inc(i);
   end;
   Result := '*' + Result + '*';
+end;
+
+function FindPara(const Source: string; Sub: String; FirstSpace: boolean = True): string;
+begin
+  Result:= FindPara(Source, [Sub], FirstSpace);
 end;
 
 { TForm1 }
@@ -59,6 +69,7 @@ procedure TForm1.FormCreate(Sender: TObject);
 begin
   Memo1.Clear;
   Edit1.Text := '/usr/bin/avrdude -C/etc/av-prdude.conf -v -x           -p           atmega328p -c arduino -P/dev/ttyACM0 -b115200 -U flash:w:Project1.hex:i';
+  Edit1.Text := '/bin/python3 -yhallo -I /n4800/DATEN/Programmierung/Lazarus/Tutorials/Embedded/Xtensa/ESP8266/UP_Loader/upload.py -c esp8266 --port /dev/ttyUSB0 --baud 115200 --before default_reset --after hard_reset write_flash 0x0 /n4800/DATEN/Programmierung/Lazarus/Tutorials/Embedded/Xtensa/ESP8266/Erster_Versuch/Project1.bin';
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
@@ -66,13 +77,16 @@ var
   s: string;
 begin
   s := Edit1.Text;
-  Memo1.Lines.Add(FindPara(s, '-C'));
-  Memo1.Lines.Add(FindPara(s, '-p'));
-  Memo1.Lines.Add(FindPara(s, '-c'));
-  Memo1.Lines.Add(FindPara(s, '-P'));
-  Memo1.Lines.Add(FindPara(s, '-b'));
-  Memo1.Lines.Add(FindPara(s, '-U'));
-  Memo1.Lines.Add(FindPara(s, '-x'));
+  Memo1.Lines.Add(FindPara(s, '-y'));
+  Memo1.Lines.Add(FindPara(s, ['-p']));
+  Memo1.Lines.Add(FindPara(s, ['-c']));
+  Memo1.Lines.Add(FindPara(s, ['-P']));
+  Memo1.Lines.Add(FindPara(s, ['-b']));
+  Memo1.Lines.Add(FindPara(s, ['-U']));
+  Memo1.Lines.Add(FindPara(s, ['-x']));
+  Memo1.Lines.Add(FindPara(s, ['--chip', '-c']));
+  Memo1.Lines.Add(FindPara(s, ['--port', '-p']));
+  Memo1.Lines.Add(FindPara(s, ['--baud', '-b']));
 end;
 
 
