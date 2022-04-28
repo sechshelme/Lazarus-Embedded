7a767bf2c7cf8605ab4b94df42ccff4f96cd931c
sechshelme <m_burkhard@gmx.ch>
2022-04-28 15:56:47 +0200

    attiny5

---
+++ AVR/ATtiny5/Blink/Project1.pas
@@ -0,0 +1,35 @@
+// Default
+
+program Project1;
+
+{$H-,J-,O-}
+
+uses
+  intrinsics;
+
+const
+  sl = 5000;  // Verzögerung
+
+  procedure mysleep(t: int32); // Ein einfaches Delay.
+  var
+    i: Int32;
+  begin
+    for i := 0 to t do begin
+      avr_nop;
+    end;
+  end;
+
+begin
+  // Setup
+  DDRB:=$FF;
+
+  repeat
+    // Loop;
+    PORTB := $FF;
+    mysleep(sl);
+
+    PORTB := $00;
+    mysleep(sl);
+
+  until false;
+end.
---
+++ Lazarus_AVR_ARM_Embedded_GUI_Package/Templates/AVR/ATtiny5/Blink_Timer.pas
@@ -0,0 +1,31 @@
+program Project1;
+
+{$H-,J-,O-}
+
+uses
+  intrinsics;
+
+  procedure Timer0_Interrupt; public Name 'TIM0_COMPA_ISR'; interrupt;
+  begin
+    TCNT0 := 10;
+    PORTB := not PORTB;
+  end;
+
+
+begin
+  // Setup
+  DDRB := $FF;
+
+  // -- Interupt unterbinden.
+  avr_cli;
+  // -- Timer0 initialisieren.
+  TCCR0A := 0;
+  TCCR0B := %010;                     // CPU-Takt / 1024
+  TIMSK0 := TIMSK0 or (1 shl OCIE0A); // Timer0 soll Interrupt auslösen.
+
+  // -- Interrupt aktivieren.
+  avr_sei;
+  repeat
+    // Loop;
+  until False;
+end.
