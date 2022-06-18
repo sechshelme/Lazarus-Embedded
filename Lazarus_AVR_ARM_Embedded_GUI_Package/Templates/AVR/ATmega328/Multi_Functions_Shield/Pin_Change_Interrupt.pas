55c1c332b726d280ca39d8e18270372a1d01adb0
sechshelme <m_burkhard@gmx.ch>
2022-06-18 17:58:45 +0200

    pin_change

--- Lazarus_AVR_ARM_Embedded_GUI_Package/Templates/AVR/ATmega328/Multi_Functions_Shield/Pin_Change_Interrupt.pas
+++ Lazarus_AVR_ARM_Embedded_GUI_Package/Templates/AVR/ATmega328/Multi_Functions_Shield/Pin_Change_Interrupt.pas
@@ -1,4 +1,4 @@
-// Multi Function Shield - 3 Button
+// Multi Function Shield - PIN Change Interrupt
 
 program Project1;
 
@@ -17,9 +17,11 @@ var
   LED_port:    TPin absolute PORTB;
   Button_port: TPin absolute PINC;
 
+// Der Interrupt, welcher ausgelöst wird, sobald eine Änderung bei den vorgebenen Tasten vorgenommen wird.
+// Überprüfen muss man selbst, was geändert wurde.
+
 procedure PC_Int1_Interrupt; public Name 'PCINT1_ISR'; interrupt;
 begin
-  // mache Irgendwas
   LED_port.P2 := Button_port.P1;
   LED_port.P3 := Button_port.P2;
   LED_port.P4 := Button_port.P3;
@@ -30,16 +32,21 @@ begin
   // BP2 = D4, BP3 = D3, BP4 = D2
   // LED schalten inventiert, da Anode an VCC !
 
-  PCICR:=%010;
-  PCMSK1:=%00001110;
+  // Interrupt für PortC aktivieren.
+  PCICR  := %010;
 
+  // Port CP1, CP2, CP3 für Interrupt aktivieren.
+  PCMSK1 := %00001110;
+
+  // Interrupt aktivieren
   avr_sei;
 
-
+  // LED-Port auf Output
   LED_ddr.P2 := True;
   LED_ddr.P3 := True;
   LED_ddr.P4 := True;
 
+  // Alle LED aus
   LED_port.P2 := True;
   LED_port.P3 := True;
   LED_port.P4 := True;
